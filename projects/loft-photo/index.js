899a24b5fd48ad4ccb67a8aadf6d5a8d976b1a33
Андрей <a00941372@gmail.com>
2024-02-07 13:59:50 +0500

    проект 3я неделя доработки

--- projects/loft-photo/index.js
+++ projects/loft-photo/index.js
@@ -1,26 +1,10 @@
 import pages from './pages';
-import './styles.css';
-import loginPage from './LoginPage';
+import('./styles.css');
+import model from './model';
 
-const photos = require('./photos.json');
-const friends = require('./friends.json');
+const pageNames = ['login', 'main', 'profile'];
 
-// const pageNames = ['login', 'main', 'profile'];
-
-const pageName = 'login';
-pages.openPage(pageName);
-loginPage.handleEvents();
-function getRandomElement(array) {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
-
-function getNextPhoto() {
-  const user = getRandomElement(friends);
-  const idxFriends = friends.indexOf(user);
-  return {
-    name: user.first_name,
-    url: getRandomElement(Object.assign(photos)[idxFriends]).url,
-  };
-}
-getNextPhoto();
+document.addEventListener('click', () => {
+  const pageName = model.getRandomElement(pageNames);
+  pages.openPage(pageName);
+});
